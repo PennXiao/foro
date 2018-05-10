6b62d57406879ebfcc741427cea9032a5cccf2de
penn <pennilessfor@gmail.com>
2018-05-11 00:33:29 +0800

    项目目录

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -6,12 +6,34 @@ namespace Acme\Controller;
 */
 
 class HomeController extends Controller{
+
 	public function index(){
 		// 获取热门前十的文章
 		$this->db();
 		$res = $this->db::table('content')->get();
-	
 		$this->view('index.html',['data'=>$res->toarray()]);
 	}
+
+	public function example($dir)
+	{
+		$this->db();
+		$res = $this->db::table('content')->get($id);
+		$this->view('content.html',['data' => $res]);
+	}
+
+	public function blong($t)
+	{
+		$this->db();
+		$res = $this->db::table('content')->get($id);
+		$this->view('content.html',['data' => $res]);
+	}
+
+	public function edit($t)
+	{
+		$this->db();
+		$res = $this->db::table('content')->get($id);
+		$this->view('content.html',['data' => $res]);
+	}
+
 
 }
--- config/route.php
+++ config/route.php
@@ -1,4 +1,12 @@
 <?php
-
+//获取热门或者最新内容 /
 $route->addRoute('GET','/','HomeController@index');
-$route->addRoute('GET','/index','HomeController@hello');
+//热门分类
+$route->addRoute('GET','/example/{dir}','HomeController@example');
+//获取博客内容 /blong?t=Pv5I6ASbUaF
+$route->addRoute('GET','/blong','HomeController@blong');
+
+// Admin 文章新增编辑
+$route->addRoute('Edit','/blong','HomeController@blong');
+
+
